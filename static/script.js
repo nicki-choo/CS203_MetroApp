c2cf9a8e82f3694989ccb2249ce803997bc12fe0
nicki-choo <270168718@yoobeestudent.ac.nz>
2023-06-09 02:31:13 +1200

    added email verification and flash message to let user know registration is successful (still going through bugs)

--- static/script.js
+++ static/script.js
@@ -34,31 +34,10 @@ document.getElementById("loginButton").addEventListener("click", handleLogin);
   })
 })()
 
-// Function to show flash message
-function showFlashMessage(message) {
-  // Create a flash message element
-  var flashMessage = document.createElement('div');
-  flashMessage.className = 'flash-message';
-  flashMessage.textContent = message;
-
-  // Append the flash message to the document body
-  document.body.appendChild(flashMessage);
-
-  // Set timeout to hide the flash message after 3 seconds
-  setTimeout(function() {
-    flashMessage.style.display = 'none';
-  }, 3000);
-}
-
-// Get the flash message from the HTML element
-var flashMessage = document.getElementById('flash-message');
-
-// Check if a flash message exists
-if (flashMessage) {
-  // Get the message content
-  var message = flashMessage.textContent.trim();
-
-  // Show the flash message
-  showFlashMessage(message);
-}
+setTimeout(function() {
+    var flashMessage = document.getElementById('flash-message');
+    if (flashMessage) {
+      flashMessage.remove();
+    }
+  }, 5000);
 
